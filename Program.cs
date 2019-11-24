27e7cd4fe00df307d851e074680d37bc9ac5019c
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-23 21:59:50 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Human
+{
+    class Human
+    {
+        // Fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+         
+        // add a public "getter" property to access health
+        public int health 
+        {
+            get { return health; }
+        }
+        // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+         
+        // Add a constructor to assign custom values to all fields
+        public Human(string name, int strength, int intelligence, int dexterity, int hp)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            health = hp;
+        }
+         
+        // Build Attack method
+        public int Attack(Human target)
+        {
+            int dmg = Strength * 5;
+            target.health -= dmg;
+            return target.health; 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
